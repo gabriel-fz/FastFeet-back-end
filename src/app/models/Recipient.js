2ccf149370f627ef152b81e6696e8d352c136d76
gabriel-fz <gabrielfzancanela@gmail.com>
2020-02-17 22:11:42 -0300

    migrations e seeds de adm e recipients

---
+++ src/app/models/Recipient.js
@@ -0,0 +1,22 @@
+import { Model, Sequelize } from 'sequelize';
+
+class Recipient extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        address: Sequelize.STRING,
+        address_number: Sequelize.INTEGER,
+        complement: Sequelize.STRING,
+        state: Sequelize.STRING,
+        city: Sequelize.STRING,
+        zip_code: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+export default Recipient;
