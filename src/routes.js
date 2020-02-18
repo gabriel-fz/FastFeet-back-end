5eb45af13546e1ed79280ed28efc6529fb47f790
gabriel-fz <gabrielfzancanela@gmail.com>
2020-02-17 23:04:22 -0300

    autenticaçao JWT concluida

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,12 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import RecipientController from './app/controllers/RecipientController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/recipients', RecipientController.store);
-
 routes.post('/user', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
