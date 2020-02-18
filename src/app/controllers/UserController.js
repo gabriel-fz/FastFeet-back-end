9441fa69b54c03e1c938841fa814913e8e9f61a1
gabriel-fz <gabrielfzancanela@gmail.com>
2020-02-17 23:29:31 -0300

    middleware de autenticaçao de rotas concluido

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -5,7 +5,7 @@ class UserController {
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (!userExists) {
-      return res.status(400).json({ error: 'User not exists.' });
+      return res.status(400).json({ error: 'User not found.' });
     }
     return res.json(userExists);
   }
--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,19 @@ import UserController from './app/controllers/UserController';
 import RecipientController from './app/controllers/RecipientController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.post('/recipients', RecipientController.store);
-routes.post('/user', UserController.store);
+// login de administrador
 routes.post('/sessions', SessionController.store);
+
+// middleware de autenticação do administrador
+routes.use(authMiddleware);
+
+// cadastro de recipients
+routes.post('/recipients', RecipientController.store);
+// listagem de administradores
+routes.post('/user', UserController.store);
 
 export default routes;
